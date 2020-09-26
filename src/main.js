f736e0f9b2316ed1afa2951260e0840a13b34bf3
845alex845 <alexunmsm2016@gmail.com>
2020-09-25 21:40:05 -0500

    home

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-
+import "@/assets/styles/index.scss"
 Vue.config.productionTip = false
 
 new Vue({
